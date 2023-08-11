5b7081707cd33958511a5bfe97048fba7287295c
Sonofben <bensonjosepholajide11@gmail.com>
2023-08-10 17:55:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n");
+return (1);
+}
