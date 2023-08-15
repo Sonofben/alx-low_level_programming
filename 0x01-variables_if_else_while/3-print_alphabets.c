b9cf36e4c5f95ffb1ee835d301098bb7c2ae525e
Sonofben <bensonjosepholajide11@gmail.com>
2023-08-14 19:38:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,21 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the alphabet in lowercase,
- * and then in uppercase, followed by a new line, using the putchar function.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	char letter;
 
-	for (1 = 0; 1 < 52; i++)
-
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(alp[i]);
+		putchar(letter);
 	}
+	putchar('$');
 	putchar('\n');
 	return (0);
 }
