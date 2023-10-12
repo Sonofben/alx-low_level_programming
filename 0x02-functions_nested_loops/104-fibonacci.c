a79db96fe388761942704a6a037179154227243d
sonofben <bensonjosepholajide11@gmail.com>
2023-10-11 18:39:23 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Fibonacci numbers up to 98
  *
- * Description: Print the first 98 Fibonacci numbers, separated by a comma and a space.
+ * Purpose - Calculate and print the first 98 Fibonacci numbers.
  *
- * Return: Always 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    unsigned long int a = 1, b = 2, c;
-    int i;
+    unsigned long int i;
+    unsigned long int bef = 1;
+    unsigned long int aft = 2;
+    unsigned long int l = 1000000000; // 10^9 for splitting large Fibonacci numbers
+    unsigned long int bef1;
+    unsigned long int bef2;
+    unsigned long int aft1;
+    unsigned long int aft2;
 
-    printf("%lu, %lu, ", a, b);
-    for (i = 3; i <= 98; i++)
+
+    printf("%lu", bef);
+
+    for (i = 1; i < 90; i++)
     {
-        c = a + b;
-        a = b;
-        b = c;
-        
-        if (i < 98)
-            printf("%lu, ", c);
-        else
-            printf("%lu\n", c);
+        printf(", %lu", aft);
+        aft += bef;
+        bef = aft - bef;
     }
 
+    bef1 = (bef / l);
+    bef2 = (bef % l);
+    aft1 = (aft / l);
+    aft2 = (aft % l);
+    for (i = 90; i < 98; ++i)
+    {
+        printf(", %lu%09lu", aft1, aft2);
+        unsigned long int temp1 = aft1;
+        unsigned long int temp2 = aft2;
+        aft1 = aft1 + bef1;
+        bef1 = temp1;
+        aft2 = aft2 + bef2;
+        bef2 = temp2;
+    }
+
+    printf("\n");
     return (0);
 }
