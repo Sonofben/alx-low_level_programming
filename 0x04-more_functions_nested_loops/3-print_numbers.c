2e78047d697a7300c1a5678387f12ab18c4a4819
Joseph Benson <32311332+Sonofben@users.noreply.github.com>
2023-10-14 00:48:30 +0100

    Update 3-print_numbers.c
    betty

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
-include "main.h"
+#include "main.h"
 
 /**
-* print_numbers - print 0-9
-* return: void
-*/
-
+ * print_numbers - Prints numbers 0 to 9 followed by a new line
+ */
 void print_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-_putchar(c);
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
 }
-_putchar('\n');
-}
+
