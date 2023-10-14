c7b14df833be30c725536dc7a50f4827db56e40c
Joseph Benson <32311332+Sonofben@users.noreply.github.com>
2023-10-14 01:24:18 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,14 @@
 #include "main.h"
 
 /**
- * print_square - Prints a square of size n with '#' in the terminal
- * @size: The size of the square
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void print_square(int size)
+int main(void)
 {
-	int row, col;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
-
-	for (row = 0; row < size; row++)
-	{
-		for (col = 0; col < size; col++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
 }
