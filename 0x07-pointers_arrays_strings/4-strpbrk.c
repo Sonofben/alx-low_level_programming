f9451a5013d20cbfff8a8641d33f84cb05814316
Joseph Benson <32311332+Sonofben@users.noreply.github.com>
2023-10-25 06:42:52 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes.
+ * _strpbrk - Searches a string for any of a set of bytes.
  * @s: The string to search.
  * @accept: The set of bytes to search for.
  *
@@ -9,17 +9,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				return (s + i);
-			}
-		}
-	}
-	return ('\0');
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                return (s + i);
+            }
+        }
+    }
+    return ('\0');
 }
